bd8698347f5d542e48198ab1b0c9da11dc195fe3
Norberto Fernandez de la Higuera <norbertoalfa@correo.ugr.es>
2021-03-09 11:54:43 +0100

    inclusion de imgui revisado

--- opengl/geometry_shader/src/main.cpp
+++ opengl/geometry_shader/src/main.cpp
@@ -1,53 +1,22 @@
 
-
-
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
 
-
-#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
-#include <GL/gl3w.h>            // Initialize with gl3wInit()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-#include <GL/glew.h>            // Initialize with glewInit()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-#include <glad/glad.h>          // Initialize with gladLoadGL()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
-#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
-#include <glbinding/Binding.h>  // Initialize with glbinding::Binding::initialize()
-#include <glbinding/gl/gl.h>
-using namespace gl;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
-#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
-#include <glbinding/glbinding.h>// Initialize with glbinding::initialize()
-#include <glbinding/gl/gl.h>
-using namespace gl;
-#else
-#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
-#endif
-
+#include <GL/glew.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <GL/glut.h>
 #include <GL/gl.h>
 
-// About Desktop OpenGL function loaders:
-//  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
-//  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
-//  You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own
-
-// Include glfw3.h after our OpenGL definitions
-
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
 
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
 
 #include <shader.h>
 #include <camera.h>
@@ -56,7 +25,6 @@ using namespace gl;
 #include <object.h>
 
 #include <iostream>
-
 
 
 bool show_demo_window = true;
@@ -119,7 +87,7 @@ static void glfw_error_callback(int error, const char* description)
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
+void processKeyInput(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
@@ -168,6 +136,10 @@ void processInput(GLFWwindow *window)
     	loadShader = true;
     	settings.setSomeChange(true);
     }
+}
+
+void processMouseKey(GLFWwindow* window, int button, int action, int mods)
+{
     
     if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
     	perDisplace = true;
@@ -270,11 +242,11 @@ void updateUniforms(){
 int openGLInit(){
 	// glfw: initialize and configure
     const char* glsl_version = "#version 130";
-    //glfwSetErrorCallback(glfw_error_callback);
+    glfwSetErrorCallback(glfw_error_callback);
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	#ifdef __APPLE__
     	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -289,44 +261,27 @@ int openGLInit(){
         return -1;
     }
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    //glfwSetScrollCallback(window, scroll_callback);
     
-    // tell GLFW to capture our mouse
-    //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetKeyCallback(window, processKeyInput);
+	glfwSetMouseButtonCallback(window, processMouseKey);
+    glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+    
     // glad: load all OpenGL function pointers
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-
-    glfwSwapInterval(1); // Enable vsync
     
-    
-    #if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
-    bool err = gl3wInit() != 0;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-    bool err = glewInit() != GLEW_OK;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-    bool err = gladLoadGL() == 0;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
-    bool err = false;
-    glbinding::Binding::initialize();
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
-    bool err = false;
-    glbinding::initialize([](const char* name) { return (glbinding::ProcAddress)glfwGetProcAddress(name); });
-#else
-    bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
-#endif
-    if (err)
+    if (glewInit() != GLEW_OK)
     {
         fprintf(stderr, "Failed to initialize OpenGL loader!\n");
         return 1;
     }
     
+    glfwSwapInterval(1); // Enable vsync
     
     // configure global opengl state
     glEnable(GL_DEPTH_TEST);
@@ -335,8 +290,6 @@ int openGLInit(){
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
@@ -473,11 +426,6 @@ void visualizarInterfaz(){
 
     // Rendering
     ImGui::Render();
-    int display_w, display_h;
-    glfwGetFramebufferSize(window, &display_w, &display_h);
-    glViewport(0, 0, display_w, display_h);
-    //glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-    //glClear(GL_COLOR_BUFFER_BIT);
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 }
@@ -500,25 +448,21 @@ int main() {
         
     	//input
         glfwPollEvents();
-    	processInput(window);
     	
-    	settings.updateSomeChange();
         // render
-        if (settings.getSomeChange() || camera.getPosChange() || light.getColorChange()
-        	|| light.getPosChange() || object.getColorChange()) {
-        	render();
-        	for (int i=0; i<3; i++)
-        		visualizarInterfaz();
-        	glfwSwapBuffers(window);
-        	settings.setSomeChange(false);
-			object.setColorChange(false);
-			light.setColorChange(false);
-			light.setPosChange(false);
-			camera.setPosChange(false);
-        }
-        
+    	visualizarInterfaz();
+    	render();
+    	for (int i=0; i<2; i++)
+			visualizarInterfaz();
+		
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+    	glfwSwapBuffers(window);
+    	
+    	settings.setSomeChange(false);
+        settings.updateSomeChange();
         
+        if (!settings.getSomeChange())
+        	glfwWaitEvents();
     }
 	
 	// Cleanup
